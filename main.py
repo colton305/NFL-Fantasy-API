13fbf6b64f18a611c6abfea3658aa2a9c44dee7c
colton305 <colton@pambra.ca>
2021-08-23 23:16:23 -0600

    Website rankings print to text file for copy/paste into rankings browser extension

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ import rankings_parser
 parser = rankings_parser.RankingsParser()
 
 parser.parse()
+parser.generate_yahoo_rankings()
--- rankings_parser.py
+++ rankings_parser.py
@@ -1,4 +1,27 @@
-import requests
-import bs4
+import pandas as pd
 
-RANKINGS_SITE
+from config import SOUP
+
+
+class RankingsParser:
+
+    def __init__(self):
+        # Declare vars
+        self.players = []
+
+    def parse(self):
+        # Iterate through ranking table
+        for div in SOUP.find("div", class_="table-responsive"):
+            for tr in div.findAll("tr"):
+                for i, td in enumerate(tr.findAll("td")):
+                    if i == 1:
+                        self.players.append(td.text)
+
+        print(self.players)
+
+    # Generate a text file to copy/paste into yahoo chrome extension
+    def generate_yahoo_rankings(self):
+        str = "\n".join(self.players)
+        file = open("yahoo_rankings.txt", "w")
+        file.write(str)
+        file.close()
