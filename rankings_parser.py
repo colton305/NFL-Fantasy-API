adabfdf0f8c465bf3f4f37c2c10d4f11af6d1116
colton305 <colton@pambra.ca>
2021-09-04 23:19:59 -0600

    Made rankings a dict instead of list to allow for multiple types of rankings (QB, RB, WR, etc.)

--- rankings_parser.py
+++ rankings_parser.py
@@ -7,21 +7,24 @@ class RankingsParser:
 
     def __init__(self):
         # Declare vars
-        self.players = []
+        self.rankings = {}
+        for key in SOUP.keys():
+            self.rankings[key] = []
 
     def parse(self):
-        # Iterate through ranking table
-        for div in SOUP.find("div", class_="table-responsive"):
-            for tr in div.findAll("tr"):
-                for i, td in enumerate(tr.findAll("td")):
-                    if i == 1:
-                        self.players.append(td.text)
+        # Iterate through each ranking table
+        for key in SOUP.keys():
+            for div in SOUP[key].find("div", class_="table-responsive"):
+                for tr in div.findAll("tr"):
+                    for i, td in enumerate(tr.findAll("td")):
+                        if i == 1:
+                            self.rankings[key].append(td.text)
 
-        print(self.players)
+        print(self.rankings)
 
     # Generate a text file to copy/paste into yahoo chrome extension
     def generate_yahoo_rankings(self):
-        string = "\n".join(self.players)
+        string = "\n".join(self.rankings["GENERAL"])
         file = open("yahoo_rankings.txt", "w")
         file.write(string)
         file.close()
