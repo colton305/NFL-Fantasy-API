b9c12e8a2c5c270df04677a3bf444b183df8a5f7
colton305 <colton@pambra.ca>
2021-09-11 16:49:51 -0600

    Add new query_free_agents function to main loop

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import roster_functions
 from config import LEAGUE_IDS, TEAM_INDEXES
 from rankings_parser import RankingsParser
 from team_interface import Team
@@ -14,11 +15,10 @@ if __name__ == "__main__":
     teams = []
     for league_id in LEAGUE_IDS:
         leagues.append(League(league_id))
-        leagues[-1].find_free_agents()
-        print(leagues[-1].free_agents)
 
     for i, index in enumerate(TEAM_INDEXES):
         teams.append(Team(leagues[i].league_id, index))
         teams[-1].generate_roster()
         teams[-1].save_roster()
         print(teams[-1].roster)
+        roster_functions.query_free_agents(leagues[i])
